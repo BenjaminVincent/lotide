cdd424db68874a1949b994304bd3d4b6e813cdce
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-06 22:59:45 +0000

    emoji time dude

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+
+const assertEquals = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🛑 Assertion Passed: " +  actual +  " === " + expected);
+  } else console.log("✅ Assertion Failed: " + actual + " !== " + expected);
+};
+// remember to place semicolons at the end of const functions
+
+
+assertEquals("Lighthouse Labs", "Bootcamp");
+assertEquals(1, 1);
+assertEquals(5, 10);
+assertEquals("hello", "hello");
