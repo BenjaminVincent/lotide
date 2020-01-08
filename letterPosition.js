e8da5cc7fb5c767939baf68f0882668aa618bf56
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-08 20:39:08 +0000

    added letterPosition.js

---
+++ letterPosition.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = function(a1, a2) {
+  if (a1.length !== a2.length) {
+    console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${a1} !== ${a2}`);
+    return;
+  }
+
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${a1} !== ${a2}`);
+      return;
+    }
+  }
+
+  console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${a1} === ${a2}`);
+};
+
+
+const letterPositions = function(sentence) {
+  const result = {};
+
+  for (let i = 0; i < sentence.length; i++) {
+    let letter = sentence[i];
+    if (!result[letter]) {
+      result[letter] = new Array();
+    }
+    result[letter].push(i);
+  }
+  return result;
+};
+
+console.log(letterPositions("test"));
+//console.log(letterPositions("the"));
+//console.log(assertArraysEqual("test", { t: [0, 3], e: [1], s: [2]}));
