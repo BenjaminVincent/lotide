50690cfe23397e7ada47b433e82b57cee2ad5973
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-08 23:31:10 +0000

    added assertObjects.Equal.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,3 +1,5 @@
+
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}`);
@@ -13,8 +15,6 @@ const eqArrays = function(a1, a2) {
 
 };
 
-
-
 const eqObjects = (object1, object2) => {
   // want to loop thru each
   let obj1 = Object.getOwnPropertyNames(object1);
@@ -25,7 +25,6 @@ const eqObjects = (object1, object2) => {
     let val1 = object1[key];
     let val2 = object2[key];
     
-    console.log(val1, " ", val2);
     if (Array.isArray(val1)) {
       if (!eqArrays(val1, val2)) return false;
     } else if (val1 !== val2) return false;
@@ -36,8 +35,14 @@ const eqObjects = (object1, object2) => {
 };
 
 
-
-
+const assertObjectsEqual = (actual, expected) => {
+  
+  if (eqObjects(actual, expected)) {
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
 
 
 const ab = {
@@ -51,17 +56,15 @@ const ba = {
 };
 
 
-console.log(eqObjects(ab, ba));
-
-
-eqObjects(ab, ba); // => true
 
 const abc = { a: "1", b: "2", c: "3" };
-console.log(eqObjects(ab, abc)); // => false
-
 const cd = { c: "1", d: ["2", 3] };
 const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
 const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2)); // => false
+
+
+assertObjectsEqual(eqObjects(ab, ba), true);
+assertObjectsEqual(eqObjects(ab, abc), false);
+assertObjectsEqual(eqObjects(cd, dc) ,true);
+assertObjectsEqual(eqObjects(cd, cd2), false);
+
