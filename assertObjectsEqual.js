1d2ce55e3063d9e70505786926d41e561004971d
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-08 23:50:05 +0000

    fixed assertObjectsEqual.js

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -36,11 +36,12 @@ const eqObjects = (object1, object2) => {
 
 
 const assertObjectsEqual = (actual, expected) => {
+  const inspect = require('util').inspect;
   
   if (eqObjects(actual, expected)) {
-    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 
@@ -63,8 +64,10 @@ const dc = { d: ["2", 3], c: "1" };
 const cd2 = { c: "1", d: ["2", 3, 4] };
 
 
-assertObjectsEqual(eqObjects(ab, ba), true);
-assertObjectsEqual(eqObjects(ab, abc), false);
-assertObjectsEqual(eqObjects(cd, dc) ,true);
-assertObjectsEqual(eqObjects(cd, cd2), false);
+assertObjectsEqual(ab, ba);
+assertObjectsEqual(ab, abc);
+assertObjectsEqual(cd, dc);
+assertObjectsEqual(cd, cd2);
+
+
 
