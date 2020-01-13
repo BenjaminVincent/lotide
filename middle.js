c34535dea4a41a5fe110df317e1e008809bfd0ed
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-13 20:31:20 +0000

    updated middle.js, added appropriate tests

--- middle.js
+++ middle.js
@@ -1,40 +1,4 @@
 
-
-const eqArrays = function(a1, a2) {
-  if (a1.length !== a2.length) return false;
-  
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) return false;
-  } return true;
-
-};
-
-const assertArraysEqual = function(a1, a2) {
-  if (a1.length !== a2.length) {
-    console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${a1} !== ${a2}`);
-    return;
-  }
-
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${a1} !== ${a2}`);
-      return;
-    }
-    
-  }
-
-  console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${a1} === ${a2}`);
-};
-
-/*
-  ### TODO ###
-
-  [] if size is 1 or 2 return empty array
-  [] ODD: return middle of a
-  [] EVEN: return both middle values
-
-*/
-
 const middle = function (arr) {
   let result = [];
   
@@ -51,11 +15,6 @@ const middle = function (arr) {
 };
 
 
+module.exports = middle;
 
-
-console.log(middle([1, 2, 3, 4])); // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
-console.log(middle([]));
-console.log(middle([1, 2, 3])); // => [2]
-console.log(middle([1, 2, 3, 4, 5])); // => [3]
 
