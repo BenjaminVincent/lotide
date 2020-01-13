4625d6b7cf5b18b99ca4b611c2342b163c96196b
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-13 20:26:28 +0000

    added test for eqArraysTest.js assertArraysEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,16 @@
+
+const eqArrays = require('./eqArrays.js');
+
 const assertArraysEqual = function(a1, a2) {
-  if (a1.length !== a2.length) {
+  if (eqArrays(a1, a2)) {
+    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${a1} === ${a2}`);
+  } else {
     console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${a1} !== ${a2}`);
-    return;
   }
-
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${a1} !== ${a2}`);
-      return;
-    }
-    
-  }
-
-  console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${a1} === ${a2}`);
 };
 
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should pass
-assertArraysEqual([1, 2, 3], [1, 2, 3, 7]);
 
-/*
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = assertArraysEqual;
 
-*/
--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log("\x1b[32m%s\x1b[0m", `✅ Assertion Passed: ${actual} === ${expected}`);
-  } else console.log("\x1b[31m%s\x1b[0m", `🛑 Assertion Failed: ${actual} !== ${expected}`);
-};
 
 const eqArrays = function(a1, a2) {
   if (a1.length !== a2.length) return false;
@@ -14,13 +9,4 @@ const eqArrays = function(a1, a2) {
 };
 
 
-
-// Tests
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
