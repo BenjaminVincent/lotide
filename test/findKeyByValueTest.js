871004adeb1ca90ae0721a064e972b23be5f3473
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-14 00:57:30 +0000

    added testing for findKeyByValue.js

---
+++ test/findKeyByValueTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert
+const findKeyByValue = require('../findKeyByValue');
+
+
+// console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+
+describe('#findKeyByValue', () => {
+  it("", () => {
+    
+  });
+});
